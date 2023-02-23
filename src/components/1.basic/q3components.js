15a03b2808bd1d0c57c3973ed522c3f2655cea79
jacobYun97 <ydy95j@naver.com>
2023-02-24 00:40:49 +0900

    q3 해결완료

--- src/components/1.basic/q3components.js
+++ src/components/1.basic/q3components.js
@@ -1,4 +1,19 @@
-function Q3components() {
-  return <div> 🏃‍♂️ 줄넘기 ... ing </div>;
+import { useEffect, useRef } from 'react';
+
+function Q3components({ setCount }) {
+    const CountRef = useRef();
+    useEffect(() => {
+        CountRef.current = setInterval(() => {
+            setCount((prev) => prev + 1);
+        }, 2000);
+        // 컴포넌트가 처음 보였을 때
+
+        // 컴포넌트가 안보였을 때
+        return () => {
+            clearInterval(CountRef.current);
+            setCount(0);
+        };
+    }, []);
+    return <div> 🏃‍♂️ 줄넘기 ... ing </div>;
 }
 export default Q3components;
