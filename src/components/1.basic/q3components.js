f0069b3a30a6c5547c61236b4e106c192ddb1dbd
twosky0202 <leehaneul0202@naver.com>
2023-02-24 00:46:59 +0900

    q3 완료, state1 진행중

--- src/components/1.basic/q3components.js
+++ src/components/1.basic/q3components.js
@@ -1,4 +1,18 @@
-function Q3components() {
-  return <div> 🏃‍♂️ 줄넘기 ... ing </div>;
+import { useEffect, useRef } from "react";
+
+function Q3components({ count, setCount }) {
+    const Counter = useRef();
+    useEffect(() => {
+        Counter.current = setInterval(() => {
+            setCount((prev) => prev + 1);
+        }, 2000);
+
+        return () => {
+            clearInterval(Counter.current);
+            setCount(0);
+        };
+    }, []);
+    console.log(count);
+    return <div> 🏃‍♂️ 줄넘기 ... ing </div>;
 }
 export default Q3components;
--- src/hooks/2.state/state1.js
+++ src/hooks/2.state/state1.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import PlayListMock from "../../__mock__/playList.json";
 
 function State1() {
-  /* 
+    /* 
     문제 1.
 
     state를 다루기 위한 기초 문제입니다.
@@ -11,32 +12,63 @@ function State1() {
     삭제 버턴을 눌렀을 때 데이터가 삭제될 수 있도록 해주세요
   */
 
-  console.log(PlayListMock.playlist);
-  /* 데이터 콘솔에 찍어두었으니 확인해볼 것 */
+    console.log(PlayListMock.playlist);
+    /* 데이터 콘솔에 찍어두었으니 확인해볼 것 */
+    const [titleInput, setTitleInput] = useState("");
+    const [singerInput, setSingerInput] = useState("");
+    // const [addList,setAddList] = useState([]);
 
-  return (
-    <>
-      <h1>문제1</h1>
-      <ul>
-        {/* list */}
-        {/* 예시 데이터입니다 */}
-        <li>
-          <h3>Summer</h3>
-          <p>Joe Hisaishi</p>
-        </li>
-      </ul>
-      <div>
-        <p>
-          곡명 : <input />
-        </p>
-        <p>
-          가수/작곡 : <input />
-        </p>
-        <p>
-          <button>추가</button>
-        </p>
-      </div>
-    </>
-  );
+    const onChangeTitleInput = (e) => {
+        setTitleInput(e.target.value);
+    };
+    const onChangeSingerInput = (e) => {
+        setSingerInput(e.target.value);
+    };
+    console.log(titleInput);
+    console.log(singerInput);
+
+    const onAddList = () => {
+        PlayListMock.playlist.push({
+            title: titleInput,
+            signer: singerInput,
+        });
+        console.log(PlayListMock.playlist);
+    };
+    const onDeleteList = () => {};
+
+    return (
+        <>
+            <h1>문제1</h1>
+            <ul>
+                {/* list */}
+                {/* 예시 데이터입니다 */}
+                {/* <li>
+                    <h3>Summer</h3>
+                    <p>Joe Hisaishi</p>
+                </li> */}
+
+                {PlayListMock.playlist.map((item) => {
+                    return (
+                        <li key={item.signer}>
+                            <h3>{item.title}</h3>
+                            <p>{item.singer}</p>
+                            <button onClick={onDeleteList}>삭제</button>
+                        </li>
+                    );
+                })}
+            </ul>
+            <div>
+                <p>
+                    곡명 : <input onChange={onChangeTitleInput} />
+                </p>
+                <p>
+                    가수/작곡 : <input onChange={onChangeSingerInput} />
+                </p>
+                <p>
+                    <button onClick={onAddList}>추가</button>
+                </p>
+            </div>
+        </>
+    );
 }
 export default State1;
