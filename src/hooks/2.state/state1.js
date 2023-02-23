4f656ee8f5f166cf430d41f8c43a0eb565340cee
beenbin <showme0241@gmail.com>
2023-02-24 02:03:56 +0900

    데이터 롤백되서 state 1번 문제도 다시... 풀고 업로드

--- src/hooks/2.state/state1.js
+++ src/hooks/2.state/state1.js
@@ -1,4 +1,6 @@
 import PlayListMock from "../../__mock__/playList.json";
+import { useState } from "react";
+import styled from "styled-components";
 
 function State1() {
     /* 
@@ -10,9 +12,46 @@ function State1() {
     아래에 추가버튼을 눌러 목록에 리스트를 추가하고 
     삭제 버턴을 눌렀을 때 데이터가 삭제될 수 있도록 해주세요
   */
+    const [titleInput, setTitleInput] = useState();
+    const [songInput, setSongInput] = useState();
+    const [inputList, setInputList] = useState([]);
 
     console.log(PlayListMock.playlist);
     /* 데이터 콘솔에 찍어두었으니 확인해볼 것 */
+
+    const titleListInput = (e) => {
+        setTitleInput(e.target.value);
+    };
+    const songListInput = (e) => {
+        setSongInput(e.target.value);
+    };
+
+    const onAddList = () => {
+        PlayListMock.playlist.push({
+            title: titleInput,
+            signer: songInput,
+        });
+        setTitleInput("");
+        setSongInput("");
+        setInputList(PlayListMock.playlist);
+    };
+
+    const onDeleteList = () => {
+        inputList.filter(() => {});
+    };
+
+    const input_list = inputList.map((prev, index) => {
+        return (
+            <li key={index}>
+                <h3>
+                    {index + 1}. {prev.title}
+                </h3>
+                <p>
+                    {index + 1}. {prev.signer}
+                </p>
+            </li>
+        );
+    });
 
     return (
         <>
@@ -20,23 +59,40 @@ function State1() {
             <ul>
                 {/* list */}
                 {/* 예시 데이터입니다 */}
-                <li>
+                {input_list}
+
+                {/* <li>
                     <h3>Summer</h3>
                     <p>Joe Hisaishi</p>
-                </li>
+                </li> */}
             </ul>
             <div>
                 <p>
-                    곡명 : <input />
+                    곡명 : <input onChange={titleListInput} />
                 </p>
                 <p>
-                    가수/작곡 : <input />
+                    가수/작곡 : <input onChange={songListInput} />
                 </p>
-                <p>
-                    <button>추가</button>
-                </p>
+                <S.Button>
+                    <button onClick={onAddList}>추가</button>
+                    <button onClick={onDeleteList}>삭제</button>
+                </S.Button>
             </div>
         </>
     );
 }
 export default State1;
+
+export const Button = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    & > :first-child {
+        margin-right: 10px;
+    }
+`;
+
+export const S = {
+    Button,
+};
